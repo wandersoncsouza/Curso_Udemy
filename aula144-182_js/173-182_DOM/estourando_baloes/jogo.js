cb6a3286752a3cb2bc4ff2a392b25ab966651097
wandersoncsouza <53883005+wandersoncsouza@users.noreply.github.com>
2020-01-09 23:14:28 -0300

    Finalização Aula 179

--- aula144-182_js/173-182_DOM/estourando_baloes/jogo.js
+++ aula144-182_js/173-182_DOM/estourando_baloes/jogo.js
@@ -1,3 +1,5 @@
+var timerId = null; //variavél que armazena a chamada timeout
+
 function iniciaJogo(){
   //alert('Jogo Iniciado');
   var url = window.location.search;
@@ -22,13 +24,34 @@ function iniciaJogo(){
   }
 
     document.getElementById('cronometro').innerHTML = tempo_segundos;
-    var qtde_baloes = 10;
+    var qtde_baloes = 60;
     cria_baloes(qtde_baloes);
 
     //Imprimir baloes baloes_inteiros
     document.getElementById('baloes_inteiros').innerHTML = qtde_baloes;
     document.getElementById('baloes_estourados').innerHTML = 0;
+
+    contagem_tempo(tempo_segundos + 1)
+
 }
+
+function contagem_tempo(segundos){  //Inicio da função de contagem regressiva
+
+    segundos = segundos - 1;        //Decremento do segundo
+    if(segundos == -1){             //condição se para limitar a contagem
+      clearTimeout(timerId);        //Limpa a variavél timerId
+      game_over();                  //Chamada da função game_over
+      return false;                 //Condição para parar o loop
+    }
+    document.getElementById('cronometro').innerHTML = segundos;  //inserir os segundos na pagina HTML
+    timerId = setTimeout("contagem_tempo("+segundos+")", 1000);
+}
+
+function game_over(){
+    alert('Fim de jogo. Você não conseguiu estourar todos os balões a tempo');
+}
+
+
 
 function cria_baloes(qtde_baloes){
 
@@ -36,7 +59,45 @@ function cria_baloes(qtde_baloes){
     var balao = document.createElement("img");
     balao.src = 'imagens/balao_azul_pequeno.png';
     balao.style.margin = '10px';
-
+    balao.id = 'b'+i;
+    balao.onclick = function(){ estourar(this); }  // Ao clicar chamar a função estourar
     document.getElementById('cenario').appendChild(balao);
   }
+}
+
+function estourar(e){
+    var id_balao = e.id;
+    document.getElementById(id_balao).src = 'imagens/balao_azul_pequeno_estourado.png';
+    pontuacao(-1);
+}
+
+function pontuacao(acao){
+    var baloes_inteiros = document.getElementById('baloes_inteiros').innerHTML;
+    var baloes_estourados = document.getElementById('baloes_estourados').innerHTML;
+
+    baloes_inteiros = parseInt(baloes_inteiros);
+    baloes_estourados = parseInt(baloes_estourados);
+
+    baloes_inteiros = baloes_inteiros +  acao;
+    baloes_estourados = baloes_estourados - acao;
+
+    //alert(baloes_inteiros);
+    //alert(baloes_estourados);
+
+    document.getElementById('baloes_inteiros').innerHTML = baloes_inteiros;
+    document.getElementById('baloes_estourados').innerHTML = baloes_estourados;
+
+    situacao_jogo(baloes_inteiros);
+}
+
+function situacao_jogo(baloes_inteiros){
+    if(baloes_inteiros == 0){
+      alert('Parabéns! Você conseguiu estourar todos os balões a tempo.');
+      parar_jogo();
+    }
+}
+
+function parar_jogo(){
+  clearTimeout(timerId);
+
 }
