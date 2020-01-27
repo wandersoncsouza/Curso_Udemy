ec4193a2050145ea54bdcdf3174ae1563bec7850
wandersoncsouza <w_costasouza@hotmail.com>
2020-01-27 14:52:51 -0300

    14:52

--- docker/projeto2/www/index.php
+++ docker/projeto2/www/index.php
@@ -24,7 +24,8 @@
 
     echo '<table class="table table-striped">';
     echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
-    while($value = $result->fetch_array(MYSQLI_ASSOC)){echo '<tr>';
+    while($value = $result->fetch_array(MYSQLI_ASSOC)){
+        echo '<tr>';
         echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
         foreach($value as $element){
             echo '<td>' . $element . '</td>';
