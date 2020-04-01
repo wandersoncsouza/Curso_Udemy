734ba87ad268596f9d3feeb1a1e6107963682e96
wandersoncsouza <53883005+wandersoncsouza@users.noreply.github.com>
2020-03-31 23:11:17 -0300

    Atualizado registra_usuario.php
    Implementado a função para identificar se usuário e email já foram cadastrados anteriormente.

--- projetotwitterclone/twitter_clone/registra_usuario.php
+++ projetotwitterclone/twitter_clone/registra_usuario.php
@@ -11,24 +11,39 @@ $link = $objDb->conecta_mysql();        //Execução da função conecta_mysql()
 
 // Verificar se o usuario já existe
 $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario'";
-if(mysqli_query($link, $sql)){
+if($resultado_id = mysqli_query($link, $sql)){
+    
+    $dados_usuario = mysqli_fetch_array($resultado_id);
+    
+    if(isset($dados_usuario['usuario'])){
+        echo '<h2>Usuário já cadastrado. Tente outro!</h2>';
+    }else{
+        echo '<h2>Usuário não cadastrado ainda</h2>';
+    }
 
-    mysqli_fetch_array($resultado_id);
 }else{
     echo 'Erro ao tentar localizar o registro de usuário';
+}
+
+// Verificar se e-mail já existe
+$sql = "SELECT * FROM usuarios WHERE email = '$email'";
+if($resultado_id = mysqli_query($link, $sql)){
+
+    $dados_usuario = mysqli_fetch_array($resultado_id);
+
+    if(isset($dados_usuario['email'])){
+        echo '<h2>email já cadastrado</h2>';
+    } else {
+        echo '<h2>email não cadastrado ainda</h2>';
+    }
+} else {
+    echo '<h2>Erro ao tentar localizar o registro de email</h2>';
 }
 
 
 
 
-
-// Verificar se e-mail já existe
-
-
-
-
-
-
+die();
 
 $sql = "INSERT INTO usuarios(usuario, email, senha) VALUES ('$usuario', '$email', '$senha')";
 //executar a query
